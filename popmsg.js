364030c364248994c9764d22b54d4606a1fb512c
PiyushS <147948037+Saxena-PiyushOfficial@users.noreply.github.com>
2024-01-05 23:11:03 +0530

    Add files via upload

---
+++ popmsg.js
@@ -0,0 +1,18 @@
+alert("this is alert window")
+var ans=confirm("select ok or cancel")
+if(ans){
+document.write("<h1>You select ok</h1>")
+var nm=prompt("enter your name")
+document.write("name is "+nm)
+}
+else{
+document.write("<h3>You select cancel</h3>")
+
+
+
+
+  var sal=prompt("enter your sal")
+  document.write("salary is "+sal)
+
+}
+
