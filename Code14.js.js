7a5f5a2eee513bbfef17ebb612dd20b4b40e5289
PiyushS <147948037+Saxena-PiyushOfficial@users.noreply.github.com>
2024-01-19 23:28:26 +0530

    Add files via upload

---
+++ Code14.js.js
@@ -0,0 +1,4 @@
+var str=prompt("ARE YOU VISITOR OR PRESIDENT");
+
+greeting=(str=="visitor")?"Dear":"Dear President";
+alert(greeting+" how are you");
---
+++ Code15.js.js
@@ -0,0 +1,7 @@
+
+      var d=new Date();
+      var time=d.getHours();
+      if (time<2) 
+      {
+      console.log("Good morning");
+      }
